06e5f33445230618c42bc2df0d0588991b36423f
Nolen Johnson <johnsonnolen@gmail.com>
2025-09-07 17:15:00 -0400

    Initial pull from fbx8am-user 10 QTT8.201201.002 v10.5.59 release-keys

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),nicepool)
+
+$(call add-radio-file-sha1-checked,radio/bootloader.img,8dee4ccfaca369b93d3fc48652169a4a8c2e7474)
+
+endif
---
+++ nicepool-vendor.mk
@@ -0,0 +1,74 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/freebox/nicepool
+
+PRODUCT_COPY_FILES += \
+    vendor/freebox/nicepool/proprietary/odm/etc/firmware/firmware.le:$(TARGET_COPY_OUT_ODM)/etc/firmware/firmware.le \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/chmod_files.sh:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/chmod_files.sh \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/dtvkit/config.xml:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/dtvkit/config.xml \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/dtvkit/satellite.json:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/dtvkit/satellite.json \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/pq/pq.db:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/pq/pq.db \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/pq/pq_default.ini:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/pq/pq_default.ini \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/tv_default.cfg:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/tv_default.cfg \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/tv_default.xml:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/tv_default.xml \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/tv_rrt_define.xml:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/tv_rrt_define.xml \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/tv_setting_config.cfg:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/tv_setting_config.cfg \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/tvconfig.conf:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/tvconfig.conf \
+    vendor/freebox/nicepool/proprietary/odm/etc/tvconfig/tvscan.conf:$(TARGET_COPY_OUT_ODM)/etc/tvconfig/tvscan.conf \
+    vendor/freebox/nicepool/proprietary/system/etc/permissions/droidlogic-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/droidlogic-hiddenapi-package-whitelist.xml \
+    vendor/freebox/nicepool/proprietary/system/etc/sysconfig/netflix.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/netflix.xml \
+    vendor/freebox/nicepool/proprietary/system_ext/etc/permissions/droidlogic.dtvkit.software.core.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/droidlogic.dtvkit.software.core.xml \
+    vendor/freebox/nicepool/proprietary/vendor/etc/audio_policy_configuration_dolby_ms12.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_dolby_ms12.xml \
+    vendor/freebox/nicepool/proprietary/vendor/etc/framerate.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/framerate.cfg \
+    vendor/freebox/nicepool/proprietary/vendor/etc/init/init.hbg.remote.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.hbg.remote.rc \
+    vendor/freebox/nicepool/proprietary/vendor/etc/init/tee-supplicant.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/tee-supplicant.rc \
+    vendor/freebox/nicepool/proprietary/vendor/etc/init/tee_hdcp.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/tee_hdcp.rc \
+    vendor/freebox/nicepool/proprietary/vendor/etc/init/tee_key_inject.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/tee_key_inject.rc \
+    vendor/freebox/nicepool/proprietary/vendor/etc/init/tee_preload_fw.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/tee_preload_fw.rc \
+    vendor/freebox/nicepool/proprietary/vendor/etc/permissions/droidlogic.software.core.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/droidlogic.software.core.xml \
+    vendor/freebox/nicepool/proprietary/vendor/etc/permissions/droidlogic.software.netflix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/droidlogic.software.netflix.xml \
+    vendor/freebox/nicepool/proprietary/vendor/etc/permissions/droidlogic.tv.software.core.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/droidlogic.tv.software.core.xml \
+    vendor/freebox/nicepool/proprietary/vendor/etc/permissions/nrdp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/nrdp.xml \
+    vendor/freebox/nicepool/proprietary/vendor/etc/permissions/privapp-permissions-amlogic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-amlogic.xml \
+    vendor/freebox/nicepool/proprietary/vendor/etc/remote.txt:$(TARGET_COPY_OUT_VENDOR)/etc/remote.txt \
+    vendor/freebox/nicepool/proprietary/vendor/lib/teetz/00d1ca22-1764-4e35-90aa5b8c12630764.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/00d1ca22-1764-4e35-90aa5b8c12630764.ta \
+    vendor/freebox/nicepool/proprietary/vendor/lib/teetz/2c1a33c0-44cc-11e5-bc3b0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/2c1a33c0-44cc-11e5-bc3b0002a5d5c51b.ta \
+    vendor/freebox/nicepool/proprietary/vendor/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta \
+    vendor/freebox/nicepool/proprietary/vendor/lib/teetz/8efb1e1c-37e5-4326-a5d6-8c33726c7d57.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/8efb1e1c-37e5-4326-a5d6-8c33726c7d57.ta \
+    vendor/freebox/nicepool/proprietary/vendor/lib/teetz/9a04f079-9840-4286-ab92e65be0885f95.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/9a04f079-9840-4286-ab92e65be0885f95.ta \
+    vendor/freebox/nicepool/proprietary/vendor/lib/teetz/b472711b-3ada-4c37-8c2a-7c64d8af0223.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/b472711b-3ada-4c37-8c2a-7c64d8af0223.ta \
+    vendor/freebox/nicepool/proprietary/vendor/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c51b.ta \
+    vendor/freebox/nicepool/proprietary/vendor/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta \
+    vendor/freebox/nicepool/proprietary/vendor/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta
+
+PRODUCT_PACKAGES += \
+    consumerir.amlogic \
+    liboemcrypto \
+    libplayready \
+    libsecmem \
+    libsubtitlemanager_jni_vendor \
+    libtee_load_video_fw \
+    libteec \
+    libvendorfont_vendor \
+    libnetflixplugin \
+    libplayreadymediadrmplugin \
+    vendor.amlogic.hardware.subtitleserver@1.0_vendor \
+    libSubtitleClient \
+    libamavutils_system_ext \
+    libsubtitlebinder \
+    libvendorfont \
+    vendor.amlogic.hardware.subtitleserver@1.0 \
+    libHwAudio_dcvdec \
+    libHwAudio_dtshd \
+    droidlogic-res \
+    droidlogic-tv \
+    droidlogic \
+    droidlogic-dtvkit \
+    vendor.amlogic.hardware.subtitleserver@1.0.xml \
+    tee-supplicant \
+    tee_hdcp \
+    tee_key_inject \
+    tee_preload_fw
