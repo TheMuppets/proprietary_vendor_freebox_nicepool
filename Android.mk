882cc5b2fa6c8d7c43668b8305b3f5504ad5950b
Nolen Johnson <johnsonnolen@gmail.com>
2026-04-16 21:11:14 -0400

    Extract from Freebox/fbx8am/fbx8am:10/QTT8.201201.002/v10.5.66:user/release-keys

--- Android.mk
+++ Android.mk
@@ -6,6 +6,6 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),nicepool)
 
-$(call add-radio-file-sha1-checked,radio/bootloader.img,8dee4ccfaca369b93d3fc48652169a4a8c2e7474)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,6e071f47dde5a3b74093e5b69cff4be963443627)
 
 endif
